a638488554022c9aed576b9cf2d4b5e69302f3de
paymybills <aniroy.ois@gmail.com>
2026-01-17 23:42:45 +0530

    fix: Remove output:export to allow security headers

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
     reactStrictMode: true,
-    images: {
-        unoptimized: true,
-    },
     async headers() {
         return [
             {
